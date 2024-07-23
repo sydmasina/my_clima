184b55ef78c8915040c1824b262c5c0453ee8cf9
sydmasina <masinasydwell@gmail.com>
2024-07-23 16:14:06 +0200

    init env variable globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:my_clima/screens/loading_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    print(e);
+  }
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
