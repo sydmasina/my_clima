02de89286c4e6bce41e03fb7c6f96c374d719d9f
sydmasina <masinasydwell@gmail.com>
2024-07-23 20:05:10 +0200

    update to validate response statusCode

--- lib/utilities/fetch.dart
+++ lib/utilities/fetch.dart
@@ -3,7 +3,11 @@ import 'package:http/http.dart' as http;
 Future getData(String uri) async {
   try {
     var response = await http.get(Uri.parse(uri));
-    return response.body;
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      return response.body;
+    } else {
+      throw Stream.error('Something went wrong when fetching data.');
+    }
   } catch (e) {
     print(e);
   }
