b7c4926bf0f7c3a4e5420a64403f3594a4c8fc81
sydmasina <masinasydwell@gmail.com>
2024-07-25 14:52:10 +0200

    update to decode data from fetch

--- lib/utilities/fetch.dart
+++ lib/utilities/fetch.dart
@@ -1,10 +1,11 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future getData(String uri) async {
   try {
     var response = await http.get(Uri.parse(uri));
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      return response.body;
+      return jsonDecode(response.body);
     } else {
       throw Stream.error('Something went wrong when fetching data.');
     }
